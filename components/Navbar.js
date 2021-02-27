bb5a50f39e4b2a6662d76f9713d4250d0f516d21
elgrantut <tute.sanchez@gmail.com>
2021-02-27 14:31:03 -0300

    Setting up enviroment and styles

---
+++ components/Navbar.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+export default function NavBar() {
+  return (
+    <nav className='flex justify-between items-center py-4'>
+      <p className='text-2xl font-bold text-grey-800'>My Todos</p>
+      <div class='flex'>
+        <a
+          href='/api/logout'
+          className='rounded bg-blue-500 hover:bg-blue-600 text-white py-2 px-4'
+        >
+          Log Out
+        </a>
+        <a
+          href='/api/logout'
+          className='rounded bg-blue-500 hover:bg-blue-600 text-white py-2 px-4'
+        >
+          Log In
+        </a>
+      </div>
+    </nav>
+  )
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,11 @@
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div className='container mx-auto my-10 max-w-sm'>
+      <Component {...pageProps} />
+    </div>
+  )
 }
 
 export default MyApp
